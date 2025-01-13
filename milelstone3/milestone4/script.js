5cbe6e5cd46db056bb14068c200358aefdc315fb
rafayraffy <rafayhingoro57@gmail.com>
2025-01-13 20:35:10 +0500

    Add files via upload

---
+++ milelstone3/milestone4/script.js
@@ -0,0 +1,18 @@
+var _a;
+(_a = document.getElementById('resume-form')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', generateResume);
+function generateResume(event) {
+    event.preventDefault();
+    var fullName = document.getElementById('fullName').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var skills = document.getElementById('skills').value;
+    var education = document.getElementById('education').value;
+    var skillsList = skills.split(',').map(function (skill) { return "<li>".concat(skill.trim(), "</li>"); }).join('');
+    var resumeContent = "\n        <div class=\"resume-header\">\n            <h3 contenteditable=\"true\">".concat(fullName, "</h3>\n            <p contenteditable=\"true\">Email: ").concat(email, "</p>\n            <p contenteditable=\"true\">Phone: ").concat(phone, "</p>\n        </div>\n        <div class=\"resume-section\">\n            <h4>Skills</h4>\n            <ul contenteditable=\"true\">").concat(skillsList, "</ul>\n        </div>\n        <div class=\"resume-section\">\n            <h4>Education</h4>\n            <p contenteditable=\"true\">").concat(education, "</p>\n        </div>\n    ");
+    var resumeContainer = document.getElementById('resume-container');
+    var resumeContentContainer = document.getElementById('resume-content');
+    if (resumeContainer && resumeContentContainer) {
+        resumeContentContainer.innerHTML = resumeContent;
+        resumeContainer.style.display = 'block';
+    }
+}
